cf11eee7e03088b62487066d350458cd63dca60f
Prabhneet <prabhneet13@gmail.com>
2023-05-07 12:35:35 +0530

    Basic Authentication

--- API/views.py
+++ API/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from API import serializers, models
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 # Create your views here.
 
 # ------- Model Serializer ------
+
+
 class StudentViewset(viewsets.ModelViewSet):
     serializer_class = serializers.StudentSerializer
     queryset = models.Student.objects.all()
+
+    #  ------ Basic Authentication ------
+    authentication_classes = [BasicAuthentication]
+    # By default permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
+
+# AllowAny -> Anyone can access the API
+# IsAuthenticated -> User, Admin and Superuser can login
+# IsAdminUser -> Only Staff user(Admin) or Superuser can access the API
